f16d836a85b65b9c2e5ed59142069045f21593c9
jibril <jazimjibril@gmail.com>
2022-10-03 20:31:56 +0300

    made changes ⚡

---
+++ src/components/cartWidget.vue
@@ -0,0 +1,10 @@
+<!-- cart Widget -->
+<template>
+ <div class="border border-red-500 h-56 w-56">
+  <h1>Cart Widget</h1>
+ </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
