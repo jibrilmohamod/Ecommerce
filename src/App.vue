f79a8522876d3d9eca7798c48606ae56f8bcdc26
jibril <jazimjibril@gmail.com>
2022-10-03 16:02:21 +0300

    project init 🌟

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>
+  <TheNav />
+ </div>
+</template>
+
+<script setup>
+ import TheNav from "./components/TheNav.vue"
+</script>
+
+<style>
+ * {
+  font-family: "Kumbh Sans", sans-serif;
+ }
+</style>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,67 @@
+<template>
+ <div
+  class="w-10/12 m-auto h-[10vh] border border-red-600 flex items-center justify-between"
+ >
+  <nav class="border h-full">
+   <ul class="flex items-center h-full">
+    <li class="mr-14">
+     <router-link to="/"
+      ><h1 class="text-4xl font-bold">sneakers</h1></router-link
+     >
+    </li>
+    <li class="mr-14">
+     <router-link to="/about"
+      ><h2 class="text-gray-400 hover:text-black">Collections</h2></router-link
+     >
+    </li>
+    <li class="mr-14">
+     <router-link to="/about"
+      ><h2 class="text-gray-400 hover:text-black">Men</h2></router-link
+     >
+    </li>
+    <li class="mr-14">
+     <router-link to="/about">
+      <h2 class="text-gray-400 hover:text-black">Women</h2>
+     </router-link>
+    </li>
+    <li class="mr-14">
+     <router-link to="/about"
+      ><h2 class="text-gray-400 hover:text-black">About</h2>
+     </router-link>
+    </li>
+    <li class="mr-14">
+     <router-link to="/about"
+      ><h2 class="text-gray-400 hover:text-black">Contacts</h2>
+     </router-link>
+    </li>
+   </ul>
+  </nav>
+  <div class="h-full flex">
+   <!-- cart -->
+   <nav class="h-full border">
+    <ul class="flex items-center h-full">
+     <li>
+      <router-link to="/cart">
+       <Icon class="text-2xl" icon="clarity:shopping-cart-line" />
+      </router-link>
+     </li>
+     <!-- profile image -->
+     <li class="ml-14">
+      <router-link to="/profile"
+       ><img
+        class="w-14 border h-14 rounded-full"
+        src="../assets/images/image-avatar.png"
+        alt=""
+      /></router-link>
+     </li>
+    </ul>
+   </nav>
+  </div>
+ </div>
+</template>
+
+<script setup>
+ import { Icon } from "@iconify/vue"
+</script>
+
+<style lang="scss" scoped></style>
