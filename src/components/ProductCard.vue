699d452cf4a86233664699795cb7ee32a1d4a934
jibril <jazimjibril@gmail.com>
2022-10-03 20:08:18 +0300

    made changes ⚡

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,14 +1,14 @@
 <template>
- <div class="h-[90vh] border border-green-600">
-  <div class="w-9/12 border border-red-500 h-full m-auto flex">
+ <div class="h-[90vh]">
+  <div class="w-9/12 -red-500 h-full m-auto flex">
    <!-- product images -->
-   <div class="h-full w-1/2 border border-fuchsia-600 pt-32">
+   <div class="h-full w-1/2 -fuchsia-600 pt-32">
     <img
      src="../assets/images/image-product-1.jpg"
      alt=""
      class="rounded-xl w-5/6 mx-auto"
     />
-    <div class="w-5/6 border border-yellow-400 mx-auto flex mt-10">
+    <div class="w-5/6 -yellow-400 mx-auto flex mt-10">
      <img
       src="../assets/images/image-product-1-thumbnail.jpg"
       alt=""
@@ -32,7 +32,7 @@
     </div>
    </div>
    <!-- product info -->
-   <div class="h-full w-1/2 border-green-800 border-2">
+   <div class="h-full w-1/2 -green-800 -2">
     <div class="w-3/4 m-auto flex flex-col justify-center h-full gap-6">
      <h3 class="text-amber-500 font-semibold tracking-wider">
       SNEAKER COMPANY
@@ -53,26 +53,30 @@
       <h2 class="text-gray-400 mt-2 line-through">$250.00</h2>
      </div>
      <!-- add to cart and product counter -->
-     <div class="flex items-center mt-10 border justify-around">
-      <div class="flex basis-1 items-center gap-2 border border-red-500">
+     <div class="flex items-center mt-10 justify-around">
+      <div class="flex items-center gap-5 rounded-md bg-orange-50">
        <button
-        class="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
+        class="rounded-md w-10 h-10 flex items-center justify-center"
         @click="minusCounter"
        >
-        <Icon icon="ant-design:minus-outlined" />
+        <Icon
+         icon="ant-design:minus-outlined"
+         class="text-orange-600 text-xl"
+        />
        </button>
-       <h2 class="text-2xl font-bold">{{ counter }}</h2>
+       <h2 class="font-bold px-4">{{ counter }}</h2>
        <button
-        class="border border-gray-400 rounded-md w-10 h-10 flex items-center justify-center"
+        class="rounded-md w-10 h-10 flex items-center justify-center"
         @click="addCounter"
        >
-        <Icon icon="ant-design:plus-outlined" />
+        <Icon icon="ant-design:plus-outlined" class="text-orange-600 text-xl" />
        </button>
       </div>
       <button
-       class="bg-amber-500 text-white font-bold px-10 py-3 rounded-md ml-10"
+       class="bg-amber-500 text-white font-bold px-14 py-3 rounded-md flex gap-3 items-center"
       >
-       Add to Cart
+       <Icon class="text-2xl" icon="clarity:shopping-cart-line" />
+       <p>Add to cart</p>
       </button>
      </div>
     </div>
